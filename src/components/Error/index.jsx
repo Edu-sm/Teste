dac4e8f51d736d12510a40abe0f84574e121be00
Edu-sm <eduardosallesdemattos@gmail.com>
2025-10-22 14:08:50 -0300

    Update index.jsx

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -20,9 +20,9 @@ const BodyColor = styled.body`
 export default function Error404() {
     return (
         <BodyColor className="bg-light">
-            <section className="container rounded-5  d-flex flex-column text-center erro-container mt-5 justify-content-center" style={{ height: "50rem" }}>
+             <section className="container-fluid card rounded-5  d-flex flex-column text-center erro-container mt-5 justify-content-center" style={{ height: "50rem" }}>
                 <img src="/images/meteora.png" className="container pb-5"></img>
-                <div className="card justify-content-center container rounded-5 bg-dark" style={{ width: "40rem", height: "30rem" }}>
+                <div className="container-fluid card justify-content-center container rounded-5 bg-dark" style={{ width: "20rem", height: "20rem" }}>
                     <h1 className="fs-2 fw-bold cor-texto">Error 404</h1>
                     <p className="text-light fw-bold fs-5 pb-4">página não encontrada</p>
                     <Link to="/" className="text-info text-decoration-none mt-3 d-block"><button className="btn cor-botao fw-bold">Voltar para página inicial</button></Link>
@@ -30,4 +30,5 @@ export default function Error404() {
             </section>
         </BodyColor>
     )
+
 }
